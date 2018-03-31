04abc27ab6006f184f5717497d8004126e3772d5
Martial Anouman <manouman@live.fr>
2018-03-31 10:40:42 +0000

    Emit and broadcast event

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ let socket = io();
 
 socket.on('connect', () => {
 
-    socket.emit('createMessage', {
-        to: '@Andrew',
-        text: 'Hi dude !',
-    });
+    // socket.emit('createMessage', {
+    //     to: '@Andrew',
+    //     text: 'Hi dude !',
+    // });
 
 });
 
@@ -14,5 +14,17 @@ socket.on('disconnect', () => console.log('Disconnected from server.'));
 socket.on('newMessage', (message) => {
 
     console.log('New message', message);
+
+});
+
+socket.on('newUser', (message) => {
+
+    console.log('Welcome user', message);
+
+})
+
+socket.on('userJoined', (message) => {
+
+    console.log('New user joined', message);
 
 })
--- server/server.js
+++ server/server.js
@@ -18,17 +18,47 @@ const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
 
-    socket.emit('newMessage', {
-        from: '@Martial',
-        text: 'Hey girl !',
+    console.log('New user connected');
+
+    socket.emit('newUser', {
+
+        from: '@Admin',
+        text: 'Welcome to the Chat App !',
         createdAt: new Date().getTime(),
+
     });
+
+    socket.broadcast.emit('userJoined', {
+
+        from: '@Admin',
+        text: 'New user joined !',
+        createdAt: new Date().getTime(),
+
+    })
+
+    // socket.emit('newMessage', {
+    //     from: '@Martial',
+    //     text: 'Hey girl !',
+    //     createdAt: new Date().getTime(),
+    // });
 
     socket.on('createMessage', (message) => {
 
         message.createdAt = new Date().getTime();
 
         console.log('createMessage', message);
+
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime(),
+        // });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime(),
+        // });
 
     });
 
